b70aa219c540f781ec7eae67cbf7002b9dca2138
Peter Willert <peterwillert@gmail.com>
2013-11-02 20:22:26 +0100

    ++

--- ipsumomat.js
+++ ipsumomat.js
@@ -1,11 +1,31 @@
 var dict = require('./dict');
 
 var ipsumomat = function() {
+    function getRandomInt(max, min) {
+        if (min === undefined) {
+            min = 0;
+        }
+
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
     return {
-        omat: function() {
+        omat: function(sentenceLength) {
             if (dict.start && dict.start && dict.start) {
-                return dict.start[0] + " " + dict.center[3] + " " + dict.end[0];
+                var text = "";
+                var start = "";
+                var center = "";
+                var end = "";
+                if(sentenceLength === undefined) {
+                    sentenceLength = 10;
+                };
+                start = dict.start[getRandomInt(dict.start.length -1 )] + " ";
+                for(var i = 0; i <= sentenceLength; i++) {
+                    center += dict.center[getRandomInt(dict.center.length -1 )] + " ";
+                }
+                end = dict.end[getRandomInt(dict.end.length -1 )];
+
+                return start + " " + center + " " + end;
             }
         }
     };
