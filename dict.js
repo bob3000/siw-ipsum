032a37a991dc7ec207ffd4d171fed529e34087cc
Peter Willert <peterwillert@gmail.com>
2013-11-02 22:26:16 +0100

    ++

--- dict.js
+++ dict.js
@@ -1,6 +1,6 @@
-var start = ['Schwarzer', 'Junge'];
+var start = ['Schwarzer', 'Junge', 'Soll ich', 'Mach﻿ den fiesen'];
 
-var center = ['schöner', 'heißer Kaffee', 'äh', 'richtig dunkelschwarzer', 'heißer Kaffee', 'JA', 'junge', 'alde', 'das is Kaffee', 'schwarz', 'stark', 'schwarzer', 'jetzt echt', 'schwarzer Kaffee', 'wa', 'der schmeckt richtig', 'schöner', 'schwarzer heisser schöner Kaffee', 'JUNGE! Schwarzer', 'HEISSER KAFFEE'];
+var center = ['schöner', 'heißer Kaffee', 'äh', 'richtig dunkelschwarzer', 'heißer Kaffee', 'JA', 'junge', 'alde', 'das is Kaffee', 'schwarz', 'stark', 'schwarzer', 'jetzt echt', 'schwarzer Kaffee', 'wa', 'der schmeckt richtig', 'schöner', 'schwarzer heisser schöner Kaffee', 'JUNGE! Schwarzer', 'heisser KAFFEE', 'da hilft', 'mal﻿ nen ordentlicher schlag', 'in die Schnauze', 'zeige ich dir﻿', 'meinen neuen', 'Faust-Tanz', 'Hals zu'];
 
 var end = ['hm?', 'echt jetzt.', 'uns kaffee is nun ma lecker.', 'und vor allem: SCHÖN LECKER.', 'schwarz und schön lecker.', 'richtig lecker schmeckt der.', 'richtig dunkel schwarzer heisser Kaffe!'];
 
