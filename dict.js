5a607faca19f7c70370e85faf36103e4b9b48cb3
Robin Kautz <robin.kautz@devbliss.com>
2013-11-12 15:00:13 +0100

    Mein name ist

--- dict.js
+++ dict.js
@@ -1,8 +1,57 @@
-var start = ['Schwarzer', 'Junge', 'Soll ich', 'Mach﻿ den fiesen', 'Ich erhebe'];
+var start = [
+                'Schwarzer',
+                'Junge',
+                'Soll ich',
+                'Mach﻿ den fiesen',
+                'Ich erhebe',
+                'Mein Name ist'
+            ];
 
-var center = ['schöner', 'heißer Kaffee', 'äh', 'richtig dunkelschwarzer', 'heißer Kaffee', 'JA', 'junge', 'alde', 'das is Kaffee', 'schwarz', 'stark', 'schwarzer', 'jetzt echt', 'schwarzer Kaffee', 'wa', 'der schmeckt richtig', 'schöner', 'schwarzer heisser schöner Kaffee', 'JUNGE! Schwarzer', 'heisser KAFFEE', 'da hilft', 'mal﻿ nen ordentlicher schlag', 'in die Schnauze', 'zeige ich dir﻿', 'meinen neuen', 'Faust-Tanz', 'Hals zu', 'd-d-dat stottern hab i schon imma gehabt', 'mein Name ist Karls Quell', 'ich komme aus Krombach', 'mein Glas'];
+var center = [
+                'schöner',
+                'heißer Kaffee',
+                'äh',
+                'richtig dunkelschwarzer',
+                'heißer Kaffee',
+                'JA',
+                'junge',
+                'alde',
+                'das is Kaffee',
+                'schwarz',
+                'stark',
+                'schwarzer',
+                'jetzt echt',
+                'schwarzer Kaffee',
+                'wa',
+                'der schmeckt richtig',
+                'schöner',
+                'schwarzer heisser schöner Kaffee',
+                'JUNGE! Schwarzer',
+                'heisser KAFFEE',
+                'da hilft',
+                'mal﻿ nen ordentlicher schlag',
+                'in die Schnauze',
+                'zeige ich dir﻿',
+                'meinen neuen',
+                'Faust-Tanz',
+                'Hals zu',
+                'd-d-dat stottern hab i schon imma gehabt',
+                'Karls Quell',
+                'ich komme aus Krombach',
+                'mein Glas',
+                'Superidiot'
+            ];
 
-var end = ['hm?', 'echt jetzt.', 'uns kaffee is nun ma lecker.', 'und vor allem: SCHÖN LECKER.', 'schwarz und schön lecker.', 'richtig lecker schmeckt der.', 'richtig dunkel schwarzer heisser Kaffe!', 'auf den fiesesten Wichser!'];
+var end = [
+                'hm?',
+                'echt jetzt.',
+                'uns kaffee is nun ma lecker.',
+                'und vor allem: SCHÖN LECKER.',
+                'schwarz und schön lecker.',
+                'richtig lecker schmeckt der.',
+                'richtig dunkel schwarzer heisser Kaffe!',
+                'auf den fiesesten Wichser!'
+            ];
 
 var dict = {};
 dict.start = start;
